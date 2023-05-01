385c1dc8a731f37eb70018e0b7705ddf9d0aa597
Rishabh Agrawal <r.agrawal@dyninno.in>
2023-05-01 13:47:29 +0530

    worked on queries resolvers and query variable

---
+++ fakedb.js
@@ -0,0 +1,54 @@
+/*
+ collection/Table - Users 
+                    - doc1
+                    - doc2
+*/
+export const users = [
+    {
+        id:"23131",
+        firstName:"mukesh",
+        lastName:"kumar",
+        email:"mukesh@mukeshkumar.com",
+        password:"12345"
+    },
+    {
+        id:"4232",
+        firstName:"suresh",
+        lastName:"sharma",
+        email:"suresh@sureshsharma.com",
+        password:"12346"
+    },{
+        id:"2342",
+        firstName:"rishabh",
+        lastName:"agrawal",
+        email:"rishabh@rishabhagrawal.com",
+        password:"432432"
+    },
+]
+
+/*
+ collection/Table - Quotes 
+                        - doc1
+                        - doc2
+                        - etc
+*/
+
+export const quotes = [
+    {
+        name:"I turn coffee into code",
+        by:"23131"
+    },
+    {
+        name:"I am another quote",
+        by:"23131"
+    },
+    {
+        name:"If it works dont touch it",
+        by:"4232"
+    },
+    {
+        name:"If it works dont touch it just run it only",
+        by:"2342"
+    },
+
+] 
--- server.js
+++ server.js
@@ -1,16 +1,43 @@
 import {ApolloServer, gql} from 'apollo-server';
 import {ApolloServerPluginLandingPageGraphQLPlayground} from 'apollo-server-core';
+import {quotes, users} from './fakedb.js';
 
 const typeDefs = gql`
-type Query{
-    greet: String
-}
+    type Query{
+        users:[User]
+        quotes : [Quote]
+        user(id:ID!): User
+    }
+    type User {
+        id: ID!
+        firstName: String
+        lastName: String
+        email: String   
+        quotes: [Quote]
+    }
+    type Quote {
+        name: String
+        by: String
+    }
+
 `
 
 const resolvers = {
     Query : {
-        greet : () => {
-            return "Hello Rishabh"
+        users : () => {
+            return users
+        },
+        quotes : () => {
+            return quotes
+        },
+        user: (_, args) => {
+            console.log(args)
+            return users.find(user => user.id === args.id)
+        }
+    },
+    User : {
+        quotes : (user) => {
+            return quotes.filter(quote => quote.by === user.id)
         }
     }
 }
