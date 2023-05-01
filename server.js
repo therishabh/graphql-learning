bd136bda799482e42beb742ba35383ec5060320c
Rishabh Agrawal <r.agrawal@dyninno.in>
2023-05-01 12:19:17 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+import {ApolloServer, gql} from 'apollo-server';
+import {ApolloServerPluginLandingPageGraphQLPlayground} from 'apollo-server-core';
+
+const typeDefs = gql`
+type Query{
+    greet: String
+}
+`
+
+const resolvers = {
+    Query : {
+        greet : () => {
+            return "Hello Rishabh"
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins : [
+        ApolloServerPluginLandingPageGraphQLPlayground()
+    ]
+});
+
+server.listen().then(({url}) => {
+    console.log(`Server ready at ${url}`)
+})
